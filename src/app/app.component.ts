94fb9527c3f8848a141196ee7daf19fe7e65f115
Anthony Wright <alwright626@gmail.com>
2026-05-22 15:14:11 -0400

    feat: add Material toolbar shell and lazy-loaded routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
+// src/app/app.component.ts
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'nec-study-app';
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'browse',
+    loadComponent: () =>
+      import('./components/browse/browse.component').then(m => m.BrowseComponent)
+  },
+  {
+    path: 'search',
+    loadComponent: () =>
+      import('./components/search/search.component').then(m => m.SearchComponent)
+  },
+  { path: '', redirectTo: '/browse', pathMatch: 'full' }
+];
